8121998e96a936f6a2eab6ea96a142f2ad3283b7
daniel <daniel.sousa.pinese@gmail.com>
2022-08-22 04:19:00 -0300

    aulas feitas

--- src/aulas/aula11/CuriosidadeInt.java
+++ src/aulas/aula11/CuriosidadeInt.java
@@ -4,11 +4,11 @@ public class CuriosidadeInt {
 
     public static void main(String[] args) {
 
-        int var1 = 2147483647;
+        int var1 = 2147483647;//maior numero inteiro
 
         int var2 = 1;
 
-        System.out.println(var1 + var2);
+        System.out.println(var1 + var2);//printa o menor numero inteiro -214748648
 
 
     }

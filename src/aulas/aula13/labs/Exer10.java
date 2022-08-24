38c10846c92d66433bc0d63b988cc604a20cdb5a
daniel <daniel.sousa.pinese@gmail.com>
2022-08-24 00:52:23 -0300

    refazendo exercicios basicos

--- src/aulas/aula13/labs/Exer10.java
+++ src/aulas/aula13/labs/Exer10.java
@@ -10,11 +10,16 @@ public class Exer10 {
 
         Scanner scan = new Scanner(System.in);
 
+        //vai printar "Entre com a temperatura em Celsius:"
         System.out.println("Entre com a temperatura em Celsius:");
+        //a variavel c sera scaneada
         double c = scan.nextDouble();
 
+        //a variavel f ira receber o resultado de (c * 1.8) +32
         double f = (c * 1.8) + 32;
 
+        //ira printar "A temperatura "e mostrara o resultado de c " C é igual a "
+        //e mostrara o resultado de f
         System.out.println("A temperatura " + c + " C é igual a " + f + " F");
     }
 }

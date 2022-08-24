edef3b3901dbac042819f18fd7c380bc0de291ae
daniel <daniel.sousa.pinese@gmail.com>
2022-08-24 00:52:07 -0300

    refazendo exercicios basicos

--- src/aulas/aula13/labs/Exer09.java
+++ src/aulas/aula13/labs/Exer09.java
@@ -15,9 +15,11 @@ public class Exer09 {
         //a variavel f sera scaneada
         double f = scan.nextDouble();
 
-        //a
+        //a variavel c recebera 5 * o resutado de f - 32 / 9
         double c = (5 * (f-32) / 9);
 
+        //ira printar "A temperatura " e mostrara o resultado de f
+        //e ira printar de novo " F é igual a " e mostrara o resultado de c e printara "c"
         System.out.println("A temperatura " + f + " F é igual a " + c + " C");
     }
 }

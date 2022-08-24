5887a218600f9e1db884732ae4d16f8b2ac0eef5
daniel <daniel.sousa.pinese@gmail.com>
2022-08-24 00:55:54 -0300

    refazendo exercicios basicos

--- src/aulas/aula13/labs/Exer12.java
+++ src/aulas/aula13/labs/Exer12.java
@@ -10,11 +10,15 @@ public class Exer12 {
 
         Scanner scan = new Scanner(System.in);
 
+        //vai printar "Entre com a altura"
         System.out.println("Entre com a altura");
+        //a variavel altura sera scaneada
         double altura = scan.nextDouble();
 
+        //a variavel pessoaIdeal ira receber o resultado da altura(72.2 * altura) - 58
         double pesoIdeal = (72.7 * altura) - 58;
 
+        //vai printar e aparecera a conta
         System.out.println("O peso ideal é: " + pesoIdeal);
     }
 }

f5aa399867db9e1cea87c0736c51ad64fde0c568
daniel <daniel.sousa.pinese@gmail.com>
2022-08-24 01:03:56 -0300

    refazendo exercicios basicos e comentados

--- src/aulas/aula13/labs/Exer13.java
+++ src/aulas/aula13/labs/Exer13.java
@@ -10,24 +10,40 @@ public class Exer13 {
 
         Scanner scan = new Scanner(System.in);
 
+        //vai printar
         System.out.println("Entre com o valor/hora");
+        //a variavel sera scaneada
         double valorHora = scan.nextDouble();
 
+        //vai pritar
         System.out.println("Entre com a quantidade de horas trabalhadas no mês:");
+        //a variavel sera scaneada
         double horas = scan.nextDouble();
 
+        //a variavel recebera o valor/hora * horas
         double salarioBruto = valorHora * horas;
+        //a variavel recebera o resutado do (salarioBruto / 100 ) * 8
         double inss = (salarioBruto / 100) * 8;
+        //a variavel recebera o(salarioBruto / 100 ) * 5
         double sindicato = (salarioBruto / 100) * 5;
+        //a variavel recebera o(salarioBruto / 100 ) * 11
         double ir = (salarioBruto / 100) * 11;
+        //a variavel recebera o inss + sindicato + ir
         double totalDescontos = inss + sindicato + ir;
+        //a variavel recebera o salarioBruto - totalDescontos
         double salarioLiquido = salarioBruto - totalDescontos;
 
+        //vai printar e mostra
         System.out.println("Salário bruto: " + salarioBruto);
+        //vai printar e mostra
         System.out.println("INSS: " + inss);
+        //vai printar e mostra
         System.out.println("Sindicato: " + sindicato);
+        //vai printar e mostra
         System.out.println("IR: " + ir);
+        //vai printar e mostra
         System.out.println("Total descontos: " + totalDescontos);
+        //vai printar e mostra
         System.out.println("Salário líquido: " + salarioLiquido);
     }
 }

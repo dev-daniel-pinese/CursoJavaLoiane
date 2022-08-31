0ea1bd04e22b353e9f37212530cd2fe494f1e47e
daniel <daniel.sousa.pinese@gmail.com>
2022-08-31 00:28:13 -0300

    Aula 42 sobre Final

---
+++ src/aulas/aula42/Contantes.java
@@ -0,0 +1,6 @@
+package aulas.aula42;
+
+public final class Contantes {
+    public static final String URL_BLOG = "http://loiane.com";
+    public static final String CURSO_COMPLETO = "http://loiane.training";
+}
---
+++ src/aulas/aula42/Professor.java
@@ -0,0 +1,51 @@
+package aulas.aula42;
+
+public class Professor {
+    private double salario;
+    private String nomeCurso;
+
+
+    //getSalario vai puxar o salario
+    public double getSalario() {
+        return salario;
+    }
+
+    //setSalario vai dar um valor ao salario
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    //getNomeCurso  vai puxar o nomeCurso
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    //setNomeCurso vai dar um valor ao nomeCurso
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public double calcularSalarioLiquido() {
+        //vai retorna 0
+        return 0;
+    }
+
+    public String obterEtiquetaEndereco() {
+
+        String s = "Endereço do Professor: ";
+        //s += super.getEndereco();
+
+        //vai returna A variavel S
+        return s;
+    }
+
+    //@Override
+    public void imprimirEtiquetaEndereco() {
+
+        //vai print
+        System.out.println("Imprimindo endereço do Professor");
+        //estara se referindo a um atributo fora do metodo e vai printar
+        System.out.println(this.obterEtiquetaEndereco());
+
+    }
+}

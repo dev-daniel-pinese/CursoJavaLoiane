634fafed8dadfd42154840b5825652eb3525dd84
daniel <daniel.sousa.pinese@gmail.com>
2022-08-31 00:38:00 -0300

    Aula 42 sobre Final

--- src/aulas/aula42/Pessoa.java
+++ src/aulas/aula42/Pessoa.java
@@ -21,33 +21,44 @@ public final class Pessoa {
         //ele estara se referindo ao atributo fora do metodo
         this.telefone = telefone;
     }
+    //getNome vai puxar o nome
     public String getNome() {
         return nome;
     }
+    //setNome vai dar um valor ao nome
     public void setNome(String nome) {
         this.nome = nome;
     }
+    //getendereco vai puxar o endereco
     public String getEndereco() {
         return endereco;
     }
+    //setendereco vai dar um valor ao endereco
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
+    //getTelefone vai puxar o telefone
     public String getTelefone() {
         return telefone;
     }
+    //setTelefone vai dar um valor ao telefone
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+    //getCpf vai puxar o cpf
     public String getCpf() {
         return cpf;
     }
+    //setCpf vai dar um valor ao cpf
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+    //getTelefoneCelular vai puxar o telefoneCelular
     public String getTelefoneCelular() {
         return telefoneCelular;
     }
+
+    //setTelefoneCelular  vai dar um valor ao telefoneCelular
     public void setTelefoneCelular(String telefoneCelular) {
         this.telefoneCelular = telefoneCelular;
     }
